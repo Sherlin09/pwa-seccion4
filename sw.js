9ecd324bddcbabaf43f679f5e2722195d155f22f
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-09-29 19:43:03 -0500

    37 service worker - fetch event

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-console.log('hola mundo 2');
+self.addEventListener('fetch', event => {
+
+    if (event.request.url.includes('style.css')) {
+        event.respondWith(null);
+    } else {
+
+        event.respondWith(fetch(event.request));
+    }
+});
